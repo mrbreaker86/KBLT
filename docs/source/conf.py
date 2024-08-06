adf1b03f7ab10d5a95aa03c4bda1a165d4740889
Emanuel Larsson <49065494+mrbreaker86@users.noreply.github.com>
2024-08-06 14:55:42 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,7 +12,8 @@
 #
 import os
 import sys
-import sphinx_rtd_theme
+#import sphinx_rtd_theme
+import alabaster
 #sys.path.insert(0, os.path.abspath('C:/Users/Emanuel Larsson/Desktop/KBLT/KBLT_python_acquistion_scripts/KBLT_readthedocs/'))
 sys.path.insert(0, os.path.abspath('C:/Users/Emanuel Larsson/Desktop/KBLT/KBLT_python_acquistion_scripts/KBLT_readthedocs/kblt/'))
 #sys.path.insert(0, os.path.abspath('../../kblt/'))
@@ -62,8 +63,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 #
-#html_theme = 'alabaster'
-html_theme = "sphinx_rtd_theme"
+html_theme = 'alabaster'
+#html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,

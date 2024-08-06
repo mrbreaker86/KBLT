483fa54f5456c0deb76dd79a44bb2f875c4996d0
Emanuel Larsson <49065494+mrbreaker86@users.noreply.github.com>
2024-08-06 15:07:10 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,7 @@
 #
 import os
 import sys
-#import sphinx_rtd_theme
-import alabaster
+import sphinx_rtd_theme
 #sys.path.insert(0, os.path.abspath('C:/Users/Emanuel Larsson/Desktop/KBLT/KBLT_python_acquistion_scripts/KBLT_readthedocs/'))
 sys.path.insert(0, os.path.abspath('C:/Users/Emanuel Larsson/Desktop/KBLT/KBLT_python_acquistion_scripts/KBLT_readthedocs/kblt/'))
 #sys.path.insert(0, os.path.abspath('../../kblt/'))
@@ -36,8 +35,7 @@ release = '2022.12.14'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = ['sphinx.ext.autodoc']
-#extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'sphinx_rtd_theme', 'sphinx.ext.autosummary', 'nbsphinx', 'sphinxcontrib.bibtex']
-extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'alabaster', 'sphinx.ext.autosummary']
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.autodoc', 'sphinx_rtd_theme', 'sphinx.ext.autosummary', 'nbsphinx', 'sphinxcontrib.bibtex']
 
 bibtex_bibfiles = ['bibtex/news.bib',
 'bibtex/related_projects.bib',
@@ -64,8 +62,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages. See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-#html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
